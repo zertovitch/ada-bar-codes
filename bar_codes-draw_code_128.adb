250c12927d4c0f1d5a3bdea5e36a1aa93447aec0
gdemont <gdemont@b6d6801f-233b-47cb-bfd1-1386fe6339ee>
2018-06-03 13:53:12 +0000

    Added a directory for implementations

--- bar_codes-draw_code_128.adb
+++ bar_codes-draw_code_128.adb
@@ -2,5 +2,5 @@ procedure Bar_Codes.Draw_Code_128 (bc : Bar_Code; text : String) is
 begin
   --  Generated stub: replace with real body!
   pragma Compile_Time_Warning (Standard.True, "Bar_Codes.Draw_Code_128 unimplemented");
-  raise Program_Error with "Unimplemented procedure Bar_Codes.Draw_Code_128";
+  raise Program_Error; -- "Unimplemented procedure Bar_Codes.Draw_Code_128";
 end Bar_Codes.Draw_Code_128;
