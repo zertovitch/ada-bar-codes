f1b40fd622cb1ace039eb8e9086da0bcd036764e
gdemont <gdemont@b6d6801f-233b-47cb-bfd1-1386fe6339ee>
2018-06-03 09:31:49 +0000

    Initial commit with stub files

---
+++ demo/bar_code_demo.adb
@@ -0,0 +1,9 @@
+--  Ada Bar Code Demo (ABCD :-) )
+
+with Bar_Codes;  --  !!  actually we will "with" implementations in the real demo
+
+procedure Bar_Code_Demo is
+begin
+  null;  --  !!  create full HTML with SVG
+  null;  --  !!  create a PDF snippet
+end;
