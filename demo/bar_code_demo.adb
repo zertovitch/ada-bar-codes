a45eb585e9f7385137cde40560559b44515a4de6
gdemont <gdemont@b6d6801f-233b-47cb-bfd1-1386fe6339ee>
2018-06-04 19:15:50 +0000

    Added PDF implementation

--- demo/bar_code_demo.adb
+++ demo/bar_code_demo.adb
@@ -1,15 +1,17 @@
 --  Ada Bar Code Demo (ABCD :-) )
 
-with SVG_Bar_Code;
+with PDF_Bar_Code, SVG_Bar_Code;
 
 with Bar_Codes;                         use Bar_Codes;
 
 with Ada.Text_IO;                       use Ada.Text_IO;
 
 procedure Bar_Code_Demo is
-  svg : File_Type;
+  svg, pdf : File_Type;
 begin
+  --
   --  SVG demo
+  --
   Create (svg, Out_File, "bar_code.svg");
   Put_Line (svg, "<?xml version=""1.0"" encoding=""UTF-8""?>");
   Put_Line (svg, "<!DOCTYPE svg");
@@ -19,6 +21,13 @@ begin
     SVG_Bar_Code (Code_128, (0.0, 0.0, 57.0, 23.0), "mm", "Hello World!")
   );
   Close (svg);
-  --  PDF snippet
-  null;  --  !!  create a PDF snippet
-end;
+  --
+  --  PDF snippet to be included into a PDF document.
+  --  For instance, use Insert_Graphics_PDF_Code of PDF_Out, http://apdf.sf.net/
+  --
+  Create (pdf, Out_File, "bar_code_pdf.txt");
+  Put_Line (pdf,
+    PDF_Bar_Code (Code_128, (0.0, 0.0, 57.0, 23.0), "Hello World!")
+  );
+  Close (pdf);
+end Bar_Code_Demo;
--- impl/pdf_bar_code.ads
+++ impl/pdf_bar_code.ads
@@ -1,9 +1,8 @@
 with Bar_Codes;
 
-function SVG_Bar_Code (
+function PDF_Bar_Code (
   kind     : Bar_Codes.Kind_Of_Code;
   bounding : Bar_Codes.Box;           --  Box in which the bar code should fit
-  unit     : String;                  --  length unit, for instance "mm" for millimeter
   text     : String                   --  Text to encode
 )
 return String;
